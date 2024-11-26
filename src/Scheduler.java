5bc4c669a4d837ae63f49da5e9999755f01d52a5
Sofiia Mikhaylova <sophia04mikhaylova@gmail.com>
2024-11-26 04:26:45 +0200

    Add func to calculate response time and processing time for FIFO and SJF

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,2 +1,10 @@
 public class Scheduler {
+    // Функція для обчислення часу відгуку і часу обробки для FIFO та SJF
+    public static void calculateTimes(int[] tasks, String scheduler) {
+        int n = tasks.length;
+        int[] responseTime = new int[n];
+        int[] turnaroundTime = new int[n];
+        int currentTime = 0;
+
+    }
 }
