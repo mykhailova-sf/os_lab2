df4749606936e77d4692133bea515f3c0bf341dd
Sofiia Mikhaylova <sophia04mikhaylova@gmail.com>
2024-11-26 04:31:59 +0200

    Add code FIFO and SJF for every task

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -6,5 +6,21 @@ public class Scheduler {
         int[] turnaroundTime = new int[n];
         int currentTime = 0;
 
+        // FIFO та SJF: виконуються завдання по черзі, адже всі мають однакову довжину
+        if (scheduler.equals("FIFO") || scheduler.equals("SJF")) {
+            for (int i = 0; i < n; i++) {
+                // Час відгуку: час, коли завдання починає виконуватись
+                responseTime[i] = currentTime;
+                // Час обробки: час від прибуття до завершення
+                turnaroundTime[i] = currentTime + tasks[i];
+                currentTime += tasks[i]; // наступне завдання почнеться після поточного
+            }
+        }
+
+        // Виводимо для кожного завдання
+        for (int i = 0; i < n; i++) {
+            System.out.println("Завдання " + (i + 1) + " - Час відгуку: " + responseTime[i] + ", Час обробки: " + turnaroundTime[i]);
+        }
+
     }
 }
